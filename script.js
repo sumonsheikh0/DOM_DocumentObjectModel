90c4effd6d5d620bf212f40b64c8e33588df2614
sumonsheikh0 <sumon214579@gmail.com>
2026-04-13 20:05:28 -0600

    inner text chnage

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const head = document.getElementsByClassName("head");
 const about =document.getElementsByTagName("p");
 
+const day2Learnig =document.getElementById("day2")
+
+//day2Learnig.innerText='Hello inner text now change'
+console.log(day2Learnig);
+console.log(day2Learnig.innerText);
